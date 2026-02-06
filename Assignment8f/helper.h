2341df7b1d79a9102512cc1429236e55d7210a58
Shruti-2306 <131479967+Shruti-2306@users.noreply.github.com>
2026-02-06 15:52:36 +0000

    Add files via upload

---
+++ Assignment8f/helper.h
@@ -0,0 +1,9 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+int Add(int a, int b);
+int Sub(int a, int b);
+int Mul(int a, int b);
+int Div(int a, int b);
+
+#endif
---
+++ Assignment8f/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "helper.h"
+
+int main() {
+    int a = 10, b = 5;
+
+    printf("Add: %d\n", Add(a, b));
+    printf("Sub: %d\n", Sub(a, b));
+    printf("Mul: %d\n", Mul(a, b));
+    printf("Div: %d\n", Div(a, b));
+
+    return 0;
+}
