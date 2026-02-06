c015497114632c7239061779b1432ac28db2b070
Shruti-2306 <131479967+Shruti-2306@users.noreply.github.com>
2026-02-06 15:46:18 +0000

    Add files via upload

---
+++ Assignment8b/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Addition: %d\n", a + b);
+
+    return 0;
+}
